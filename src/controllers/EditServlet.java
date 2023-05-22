501e82726b19a652072a3a2f6db759940f529c50
ryosuke.harada <ryosuke.nnlife@outlook.jp>
2023-05-22 11:41:49 +0900

    DestroyServlet.javaの実装

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -37,7 +37,7 @@ public class EditServlet extends HttpServlet {
 
 		em.close();
 
-		request.setAttribute("tasks", t);
+		request.setAttribute("task", t);
 		request.setAttribute("_token", request.getSession().getId());
 
 		request.getSession().setAttribute("task_id", t.getId());
